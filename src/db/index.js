07a24136abadd5f5d954596d9c92b884ba2dfc00
Bishnu Prasad Rath <rbishnu604@gmail.com>
2026-02-15 20:17:08 +0530

    Connected to database

---
+++ src/db/index.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+import express from "express";
+
+const app = express();
+
+const connectDB  = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connecter successfully to ${connectionInstance.connection.host} \n`);
+        app.on("error",(error)=>{
+            console.log("there is something wrong with server",error);
+            throw error
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server is running on port  ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("MongoDb connection failed",error);
+        process.exit(1); 
+    }
+}
+
+export default connectDB;
