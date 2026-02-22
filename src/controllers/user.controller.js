de43a2130ed2c36d1b7f26f308cbd8e6e8ac5439
Bishnu Prasad Rath <rbishnu604@gmail.com>
2026-02-22 13:19:26 +0530

    fix all register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,11 +4,11 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-const registerUser = asyncHandler(async(req,res,next)=>{
+const registerUser = asyncHandler(async(req,res)=>{
 // Get user details from frontend
 // Validation - Not empty
 // Check if user already exists :  Check with username and email
-// Chech for images and check for avatar
+// Check for images and check for avatar
 // upload them to cloudinary , avatar checking
 // Create user object - create entry in DB
 // remove password and refresh token field from response
@@ -22,7 +22,7 @@ if([fullName,email,username,password].some((field) => field?.trim() === "")){
     throw new ApiError(400,"All fields are required")
 }
 
-const existedUser = User.findOne({
+const existedUser = await User.findOne({
     $or : [{username},{email}]
 })
 
@@ -30,8 +30,18 @@ if(existedUser){
     throw new ApiError(409,"User with username or email already exists")
 }
 
-const avatarLocalPath = req.files?.avatar[0]?.path;
-const coverImageLocalPath = req.files?.coverImage[0]?.path;
+console.log(req.files);
+
+
+const avatarLocalPath = req.files?.avatar[0]?.path; 
+// const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+let coverImageLocalPath ; 
+
+if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length === 0){
+    coverImageLocalPath = req.files?.coverImage[0].path
+}
+
 
 if(!avatarLocalPath){
     throw new ApiError(400,"Avatar is required")
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,5 @@
-import {v2 as cloudinary} from 'cloudinary';
-import fs from 'fs';
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
 
     cloudinary.config({ 
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
@@ -15,7 +15,7 @@ import fs from 'fs';
                  });
                  console.log("Upload Result:", response);
                  //File has been uploaded successfully, now we can remove the file from local storage
-                //  fs.unlinkSync(localFilePath);
+                 fs.unlinkSync(localFilePath);
                  return response;
              } catch (error) {
                  fs.unlinkSync(localFilePath);   // remove the locally saved temp file in case of an error as well
