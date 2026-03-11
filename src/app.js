b9edeb295a8e40f93c26ba7f6798c1f8e9472abd
Bishnu Prasad Rath <rbishnu604@gmail.com>
2026-03-12 01:08:07 +0530

    add playlist functionality: implement playlist controller and routes for managing playlists

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ import videoRouter from "./routes/video.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import likeRouter from "./routes/like.routes.js"
 import commentRouter from "./routes/comment.routes.js"
-
+import playlistRouter from "./routes/playlist.routes.js"
 
 
 //Routes delcaration
@@ -39,7 +39,8 @@ app.use("/api/v1/users",userRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/like", likeRouter);
-app.use("/api/v1/comment",commentRouter)
+app.use("/api/v1/comment",commentRouter);
+app.use("/api/v1/playlist",playlistRouter)
 
 
 app.use(errorHandler);
